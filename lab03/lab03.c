9bdbfe33096593de7c2322ac39888bcbf478f745
Giovanna Gennari <giov.gennari@hotmail.com>
2022-09-06 15:45:31 -0300

    hex to dec

--- lab03/lab03.c
+++ lab03/lab03.c
@@ -92,19 +92,18 @@ void dec_to_bin(int dec, int n){
 //expandir cada dígito hexa em quatro dígitos binários, tam é a qtde de dígitos no numero hex 
 void hex_to_bin(char *hex, int tam){
   int x;
+
   for (int i = 0; i < tam; i++){
     //converter cada dígito para binário 
     x = hex[i];
     printf("num é %d\n", x);
     //se for numero
     if(48 <= x && x <= 57){
-        printf("Reconheceu numero");
-        dec_to_bin(hex[i] - 48, 4);
+        dec_to_bin(x - 48, 4);
     }
     //se for letra
     if (65 <= x && x <= 71){
-        printf("Reconheceu letra");
-        dec_to_bin(hex[i] - 47, 4);
+        dec_to_bin(x - 47, 4);
     }
   }
   
@@ -120,19 +119,19 @@ int bin_to_2complement(){
 
 //passa por cada dígito, multiplicando pela potência e somando
 void hex_to_dec(char *hex, int tam){
-    int total = 0; 
-    int x; 
+    int total = 0, p = 1, x;
 
-    for(int i = 0; i < tam; i++){
+    for(int i = tam-1; i >= 0; i--){
       x = hex[i];
       //se for numero
       if(48 <= x && x <= 57){
-        total += (hex[i] -48) * (16 ** i);
+        total = total + ((x-48) * p);
       }
       //se for letra
       if (65 <= x && x <= 71){
-          total +=  (hex[i] - 50) * (16 ** i);
+          total =  total + ((x - 55) * p);
       }
+      p = p * 16;
     }
     printf("%d", total);
 }
